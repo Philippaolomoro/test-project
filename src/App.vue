7ae328d1ac016bee7031243e332b84812022e432
Olomoro Philippa <olomoro.philippa@gmail.com>
2021-01-08 15:46:05 +0100

    create a new vue project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <MainPage/>
+  </div>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+export default {
+  name: 'app',
+  components: {
+    MainPage
+  }
+}
+</script>
+
+<style>
+body{
+  background: linear-gradient(to right, #c20049 50%, #d10061 50% 100%);
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
+}
+#app { 
+  background-size: cover;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-page" id="MainPage">
+    <h1 class="heading-name">{{info.name}}</h1>
+    <div>
+      <div>
+        <img src="" alt="">
+      </div>
+      <div>
+        <ul>
+          <li><a href=""><i class="fab fa-instagram"></i></a></li>
+          <li><a href=""><i class="fab fa-twitter"></i></a></li>
+          <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+          <li><a href=""><i class="fas fa-envelope-square"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+export default {
+  name:"MainPage",
+  data(){
+    return {info:null}
+  },
+  mounted()
+  {
+    Vue.axios.get("https://hirng-x2021.glitch.me/api")
+    .then(response => {
+      this.info = response.data
+    })
+  }
+}
+</script>
+
+<style>
+  .heading-name{
+    margin: 0 auto;
+  }
+</style>
